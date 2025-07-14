91b0b42870be7ee45cba61aeb8c8c1ca102759ff
James Munns <james@onevariable.com>
2025-07-14 17:39:24 +0200

    Update bbq2 dep, add rp2040-null demo (#50)
    * Update bbq2 dep, add rp2040-null demo
    
    * Update CI
    
    * Fix toolchains
    
    * What are doin, toolchains?
    
    * Housekeeping

--- checkall.sh
+++ checkall.sh
@@ -9,3 +9,4 @@ cargo check --manifest-path=./demos/ergot-nusb-router/Cargo.toml
 cargo check --manifest-path=./demos/ergot-router/Cargo.toml
 cargo check --target=thumbv7em-none-eabi --manifest-path=./demos/nrf52840-eusb/Cargo.toml
 cargo check --target=thumbv7em-none-eabi --manifest-path=./demos/nrf52840-null/Cargo.toml
+cargo check --target=thumbv6m-none-eabi --manifest-path=./demos/rp2040-null/Cargo.toml
