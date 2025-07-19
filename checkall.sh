f2d7c1c236d343164fb7e7aa61c476555540503c
James Munns <james@onevariable.com>
2025-07-19 19:21:40 +0200

    Condense demos into workspaces (#56)
    Condense nrf52840 and std demos into workspaces, like the rp2040 in #55

--- checkall.sh
+++ checkall.sh
@@ -4,10 +4,6 @@ set -euxo pipefail
 
 cargo check --features=std --manifest-path=./crates/ergot-base/Cargo.toml
 cargo check --features=std --manifest-path=./crates/ergot/Cargo.toml
-cargo check --manifest-path=./demos/ergot-client/Cargo.toml
-cargo check --manifest-path=./demos/ergot-nusb-router/Cargo.toml
-cargo check --manifest-path=./demos/ergot-router/Cargo.toml
-cargo check --target=thumbv7em-none-eabi --manifest-path=./demos/nrf52840-eusb/Cargo.toml
-cargo check --target=thumbv7em-none-eabi --manifest-path=./demos/nrf52840-null/Cargo.toml
-cargo check --target=thumbv6m-none-eabi --manifest-path=./demos/rp2040-null/Cargo.toml
-cargo check --target=thumbv6m-none-eabi --manifest-path=./demos/rp2040-eusb/Cargo.toml
+cargo check --all --manifest-path=./demos/std/Cargo.toml
+cargo check --all --target=thumbv7em-none-eabi --manifest-path=./demos/nrf52840/Cargo.toml
+cargo check --all --target=thumbv6m-none-eabi --manifest-path=./demos/rp2040/Cargo.toml
