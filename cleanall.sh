2aa0672083de080f925d92e67f5a77666bd0d108
James Munns <james@onevariable.com>
2025-09-04 15:40:19 +0200

    Add a "cleanall" script (#128)

---
+++ cleanall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# clean all the crates
+cargo clean --manifest-path=./crates/ergot/Cargo.toml
+cargo clean --manifest-path=./crates/cobs-acc/Cargo.toml
+
+# clean all the demo workspaces
+cargo clean --manifest-path=./demos/shared-icd/Cargo.toml
+cargo clean --manifest-path=./demos/shared-icd/Cargo.toml
+cargo clean --manifest-path=./demos/std/Cargo.toml
+cargo clean --manifest-path=./demos/nrf52840/Cargo.toml
+cargo clean --manifest-path=./demos/rp2040/Cargo.toml
+cargo clean --manifest-path=./demos/esp32c6/Cargo.toml
+cargo clean --manifest-path=./demos/rp2350/Cargo.toml
+cargo clean --manifest-path=./demos/stm32f303vc/Cargo.toml
