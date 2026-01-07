84e8e4c239b56e9ced5bdf7741e99a366036941c
Hanno Braun <hello@hannobraun.com>
2026-01-07 13:44:38 +0100

    Remove redundant `README.md` from `crates/ergot/` (#177)
    The redundant README had already fallen out of date with the one at the
    repository root, which has been actively improved. Yet, it was being
    used on `crates.io`, and included in the crate documentation.
    
    Updated both `Cargo.toml` and the crate documentation, to point to the
    remaining README instead.
    
    Maybe it would have been nice to leave a small README there that refers
    to the top-level one, for visitors of the repository. But in this case,
    Cargo would ignore the README linked from `Cargo.toml`, in favor of the
    one in the directory. (Tested with `cargo publish --dry-run`.)

--- crates/ergot/src/lib.rs
+++ crates/ergot/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../README.md")]
+#![doc = include_str!("../../../README.md")]
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 #![allow(clippy::uninlined_format_args)]
 
